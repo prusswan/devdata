a92b64710832fa9030550e3d88afd08e5c21c5d4
prusswan <prusswan@gmail.com>
2014-06-23 22:49:43 +0800

    implemented input processing and result table sorting

--- server.R
+++ server.R
@@ -1,31 +1,52 @@
 library(shiny)
 
 
-Team <- c('Germany','United States','Ghana','Portugal')
-Played <- c(3,3,3,3)
-W <- c(1,1,0,0)
-D <- c(1,1,1,1)
-L <- c(0,0,1,1)
-GF <- c(6,4,3,2)
-GA <- c(2,3,4,6)
 
 
-
-df = data.frame(Team,Played,W,D,L,GF,GA)
-df$GD <- with(df, GF - GA)
-df$Pts <- with(df, W * 3 + D)
-
-
-shinyServer(
-  function(input, output){
-        
-    output$mytable1 = renderDataTable(
-      df,
-      options = list(bFilter=0, bSort=0, bProcessing=0, bPaginate=0, bInfo=0, bSearchable=0,
-        aoColumns = 
-          list(list(bSearchable=0), list(bSearchable=0), list(bSearchable=0), list(bSearchable=0), 
-              list(bSearchable=0),list(bSearchable=0), list(bSearchable=0), list(bSearchable=0), list(bSearchable=0))
-      )
+shinyServer(function(input, output){
+  
+  computeTable <- reactive({
+    Team <- c('Germany','United States','Ghana','Portugal')
+    Played <- c(3,3,3,3)
+    W <- c(1,1,0,0)
+    D <- c(1,1,1,1)
+    L <- c(0,0,1,1)
+    GF <- c(6,4,3,2)
+    GA <- c(2,3,4,6)
+    
+    t1 <- input$goalGER
+    t2 <- input$goalUSA
+    t3 <- input$goalGHA
+    t4 <- input$goalPOR
+    
+    netW <- c(t1 > t2, t2 > t1, t3 > t4, t4 > t3)
+    netD <- c(t1 == t2, t2 == t1, t3 == t4, t4 == t3)
+    netL <- c(t1 < t2, t2 < t1, t3 < t4, t4 < t3)
+    
+    W <- W + netW
+    D <- D + netD
+    L <- L + netL
+    
+    netGF <- c(input$goalGER, input$goalUSA, input$goalGHA, input$goalPOR)
+    netGA <- c(input$goalUSA, input$goalGER, input$goalPOR, input$goalGHA)
+            
+    GF <- GF + netGF
+    GA <- GA + netGA
+    
+    df = data.frame(Team,Played,W,D,L,GF,GA)
+    df$GD <- with(df, GF - GA)
+    df$Pts <- with(df, W * 3 + D)
+    
+    df[with(df, order(-Pts,-GD,-GF)), ]    
+  })  
+          
+  output$mytable1 = renderDataTable(
+    computeTable(),
+    options = list(bFilter=0, bSort=0, bProcessing=0, bPaginate=0, bInfo=0, bSearchable=0,
+      aoColumns = 
+        list(list(bSearchable=0), list(bSearchable=0), list(bSearchable=0), list(bSearchable=0), 
+            list(bSearchable=0),list(bSearchable=0), list(bSearchable=0), list(bSearchable=0), list(bSearchable=0))
     )
+  )
   }
 )
--- ui.R
+++ ui.R
@@ -7,31 +7,32 @@ shinyUI(
   pageWithSidebar(
     headerPanel("World Cup 2014"),
     sidebarPanel(
+      helpText("Use this calculator to determine the teams that will qualify from the group, depending on the scores in their final group games."),
       h4("Match 45"),
       fluidRow(
         column(4,
-          numericInput('goal-USA', 'United States', 0, min=0, max =10, step=1)
+          numericInput('goalUSA', 'United States', 0, min=0, max =10, step=1)
         ),
         column(1,
           br(),
           h4(":", style="text-align:center")  
         ),
         column(4,
-          numericInput('goal-GER', 'Germany', 0, min=0, max =10, step=1)
+          numericInput('goalGER', 'Germany', 0, min=0, max =10, step=1)
         )
 
       ),
       h4("Match 46"),
       fluidRow(
         column(4,
-               numericInput('goal-POR', 'Portugal', 0, min=0, max =10, step=1)
+               numericInput('goalPOR', 'Portugal', 0, min=0, max =10, step=1)
         ),
         column(1,
                br(),
                h4(":", style="text-align:center")  
         ),
         column(4,
-               numericInput('goal-GHA', 'Ghana', 0, min=0, max =10, step=1)
+               numericInput('goalGHA', 'Ghana', 0, min=0, max =10, step=1)
         )
         
       )
